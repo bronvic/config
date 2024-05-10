bb39fd5f8731b8107de0418f3f7c1d95b89ca778
Voronwe Sul <mail@voronwe.me>
2024-05-10 20:53:12 +0500

    vpn

--- home/.config/polybar/scripts/wifi_manager.sh
+++ home/.config/polybar/scripts/wifi_manager.sh
@@ -11,7 +11,23 @@ if [ "$(iwctl station wlan0 show | awk '/State/ {print $2}')" == 'connected' ]; 
 
   icon=$(generate_bar.sh "$percentage")
 
-  echo " ${icon}"
+  # Run the wg command and capture stderr
+  error=$(wg 2>&1 >/dev/null)
+
+  # Check the exit code directly
+  if wg 2>/dev/null; then
+    # If exit code is 0, no VPN active
+    echo " ${icon}"
+  else
+    # If exit code is not 0, VPN is active
+    # Trying to get VPN country
+    country=$(echo "$error" | grep -oE 'wg-[A-Za-z0-9]{2}-[A-Za-z0-9]{2}' | sed 's/wg-\([A-Za-z0-9]\{2\}\)-[A-Za-z0-9]\{2\}/\1/')
+    country_lower=$(echo "${country}" | tr '[:upper:]' '[:lower:]')
+    echo "%{F#cb4b16}󱛀 ${icon} ${country_lower}%{F-}"
+  fi
+
 else
   echo "%{F#757575}󰖪 ▁%{F-}"
 fi
+
+I want a bash script that runs wg command id echo one thing if there is stderr and another if not
