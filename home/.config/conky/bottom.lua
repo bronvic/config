0414ceacfd27d76115126af1c5be7994e0da9c79
Voronwe Sul <mail@voronwe.me>
2024-09-19 15:37:02 +0300

    fixed btm syntax

--- home/.config/conky/bottom.lua
+++ home/.config/conky/bottom.lua
@@ -1,9 +1,5 @@
 function conky_main(event)
-    if conky_window == nil then
-        return
-    end
-
     if event ~= nil and event.type == "button_up" then
-        os.execute('alacritty --command btm --color=gruvbox --hide_time --disable_click &')
+        os.execute('alacritty --command btm --theme=gruvbox --hide_time --disable_click &')
     end
 end
